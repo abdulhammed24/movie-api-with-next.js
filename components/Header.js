d3c6fa0d0357796574c4fe26f6a1e61b8cc3f7c7
abdulhammed24 <abdlhamd3@gmail.com>
2022-06-09 14:12:53 -0700

    new correction

--- components/Header.js
+++ components/Header.js
@@ -13,16 +13,16 @@ const Header = () => {
   return (
     <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
       <div className="flex flex-grow justify-evenly max-w-2xl">
-        <HeaderItems title="HOME" Icon={HomeIcon} />
+        {/* <HeaderItems title="HOME" Icon={HomeIcon} />
         <HeaderItems title="TRENDING" Icon={LightningBoltIcon} />
         <HeaderItems title="VERIFIED" Icon={BadgeCheckIcon} />
         <HeaderItems title="COLLECTIONS" Icon={CollectionIcon} />
         <HeaderItems title="SEARCH" Icon={SearchIcon} />
-        <HeaderItems title="ACCOUNT" Icon={UserIcon} />
+        <HeaderItems title="ACCOUNT" Icon={UserIcon} /> */}
       </div>
       <Image
         className="object-contain"
-        src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg"
+        src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
         width={150}
         height={80}
       />
